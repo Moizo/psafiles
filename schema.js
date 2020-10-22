65545724f5b843c1f14d71e4b468ee2ad4b23662
Moizo <69458155+Moizo@users.noreply.github.com>
2020-10-22 13:45:04 +0200

    Add files via upload

---
+++ schema.js
@@ -0,0 +1,134 @@
+//Question 1
+function Q1Yes() {
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "block";
+    document.getElementById("question2").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question2").style.opacity = "1";
+}
+function Q1No() {
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question3").style.display = "block";
+    document.getElementById("question3").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question3").style.opacity = "1";
+}
+//Question 2
+function Q2Yes() {
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("result1").style.display = "block";
+    document.getElementById("result1").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result1").style.opacity = "1";
+}
+function Q2No() {
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question4").style.display = "block";
+    document.getElementById("question4").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question4").style.opacity = "1";
+}
+//Question 3
+function Q3Yes() {
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question5").style.display = "block";
+    document.getElementById("question5").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question5").style.opacity = "1";
+}
+function Q3No() {
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question7").style.display = "block";
+    document.getElementById("question7").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question7").style.opacity = "1";
+}
+//Question 4
+function Q4Yes() {
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("result2").style.display = "block";
+    document.getElementById("result2").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result2").style.opacity = "1";
+}
+function Q4No() {
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question6").style.display = "block";
+    document.getElementById("question6").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question6").style.opacity = "1";
+}
+//Question 5
+function Q5Yes() {
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("result2").style.display = "block";
+    document.getElementById("result2").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result2").style.opacity = "1";
+}
+function Q5No() {
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("result4").style.display = "block";
+    document.getElementById("result4").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result4").style.opacity = "1";
+}    
+//Question 6
+function Q6Yes() {
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("result3").style.display = "block";
+    document.getElementById("result3").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result3").style.opacity = "1";
+}
+function Q6No() {
+    document.getElementById("question6").style.display = "none";
+}
+//Question 7
+function Q7Yes() {
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("result4").style.display = "block";
+    document.getElementById("result4").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result4").style.opacity = "1";
+}
+function Q7No() {
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "block";
+    document.getElementById("question8").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question8").style.opacity = "1";
+}
+//Question 8
+function Q8Yes() {
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("result5").style.display = "block";
+    document.getElementById("result5").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result5").style.opacity = "1";
+}
+function Q8No() {
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "block";
+    document.getElementById("question9").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question9").style.opacity = "1";
+}
+//Question 9
+function Q9Yes() {
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "block";
+    document.getElementById("question10").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question10").style.opacity = "1";
+}
+function Q9No() {
+    document.getElementById("question9").style.display = "none";
+}
+//Question 10
+function Q10Yes() {
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("result6").style.display = "block";
+    document.getElementById("result6").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result6").style.opacity = "1";
+}
+function Q10No() {
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("question11").style.display = "block";
+    document.getElementById("question11").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("question11").style.opacity = "1";
+}
+//Question 11
+function Q11Yes() {
+    document.getElementById("question11").style.display = "none";
+    document.getElementById("result7").style.display = "block";
+    document.getElementById("result7").style.WebkitTransition = 'opacity 3s ease-in-out';
+    document.getElementById("result7").style.opacity = "1";
+}
+function Q11No() {
+    document.getElementById("questio11").style.display = "none";
+}
